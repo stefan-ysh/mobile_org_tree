0d2177399bd2f85fd30ca91be81c7ce00b1b8b34
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2021-11-17 11:42:00 +0800

    Delete src directory

--- src/components/MobileTree/index.js
+++
@@ -1,5 +0,0 @@
-import MobileTree from './MobileTree';
-MobileTree.install = function (Vue) {
-    Vue.component(MobileTree.name, MobileTree); //name就是开始说的vue文件暴露出来的name名
-};
-export default MobileTree;
