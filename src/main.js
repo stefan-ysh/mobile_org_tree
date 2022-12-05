aea40b27d07674810551a9366d34f83e4413a592
yshuai <58702199+stefan-ysh@users.noreply.github.com>
2022-12-05 18:54:44 +0800

    fix:入口引入npm包

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 // dev
-import MobileOrg from "./components/MobileOrg";
+// import MobileOrg from "./components/MobileOrg";
 
 // test
-// import MobileOrg from 'mobile-org'
-// import 'mobile-org/mobile-org.css'
+import MobileOrg from 'mobile-org'
+import 'mobile-org/mobile-org.css'
 
 Vue.use(MobileOrg);
 Vue.config.productionTip = false;
